7c62e6d8f0913595801ffbd4dfff232b04f7a4be
jvacanti <jennifervacanti@gmail.com>
2018-05-16 20:36:06 -0400

    added markers

--- js/logic1.js
+++ js/logic1.js
@@ -10,27 +10,29 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
       var quakeMarkers = [];
     
       // loop through the features array
-      for (var index = 0; index < features[0].length; index++) {
+      for (var index = 0; index < features.length; index++) {
         var quake = features[index];
     
+        // var color = 
         // for each quake, create a marker and bind a popup with the quake's info
-        var quakeMarker = L.geoJson(data,{
-        pointToLayer: function (response){
-          return L.circle((features[0].geometry.coordinates[1], features[0].geometry.coordinates[0]),{
+        var quakeMarker = L.geoJson(quake,{
+        pointToLayer: function (feature, latlong){
+          return L.circleMarker(latlong,{
           fillOpacity: 0.75,
           color: "white",
           fillColor: "purple",
-          radius:(features[0].properties.mag)})
-          .bindPopup("<h3>" + features[0].properties.mag + "<h3><h3>Place: " + features[0].properties.place + "<h3>");
+          radius: feature.properties.mag * 10
+        })
+          .bindPopup("<h3>" + feature.properties.mag + "<h3><h3>Place: " + feature.properties.place + "<h3>");
         }})
         // add the marker to the quakeMarkers array
       quakeMarkers.push(quakeMarker);
       }
-      console.log(features[0].geometry.coordinates[1], features[0].geometry.coordinates[0])
+      //console.log(features[0].geometry.coordinates[1], features[0].geometry.coordinates[0])
     
       // create a layer group made from the bike markers array, pass it into the createMap function
-      return createMap(L.layerGroup, quakeMarkers).addTo(map),
-      createMarkers("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").addTo(map)
+      return createMap(L.layerGroup(quakeMarkers))
+      // createMarkers("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").addTo(map)
     }
   
     
